f4eaf4dbdeea4e8cacd10a4c5b54193170b825cc
mkokonyan <mkokonyan@gmail.com>
2022-06-12 18:14:02 +0300

    hibernate basics

---
+++ src/L03_IntroductionToHibernate/Hibernate_JPA/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("school");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student student = new Student("Pesho");
+        Student second = new Student("Gosho");
+        entityManager.persist(student);
+        entityManager.persist(second);
+
+        Student first = entityManager.find(Student.class, 1);
+        entityManager.remove(first);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+}
