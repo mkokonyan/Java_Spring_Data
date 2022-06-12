a22e0b3c1e9af42b6358cfaa543122c4139c7e5a
mkokonyan <mkokonyan@gmail.com>
2022-06-12 20:38:51 +0300

    hibernate jpa exercise

--- src/L03_IntroductionToHibernate/b_exercise/src/main/java/entities/Town.java
+++ src/L03_IntroductionToHibernate/b_exercise/src/main/java/entities/Town.java
@@ -1,33 +1,25 @@
+package entities;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name = "students")
-public class Student {
+@Table(name = "towns")
+public class Town {
+    private Integer id;
+    private String name;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "name", length = 100)
-    private String name;
-
-
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student() {}
-
-    public int getId() {
+    @Column(name = "town_id")
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
+    @Column(name = "name")
     public String getName() {
         return name;
     }
@@ -38,7 +30,7 @@ public class Student {
 
     @Override
     public String toString() {
-        return "Student{" +
+        return "Town{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
